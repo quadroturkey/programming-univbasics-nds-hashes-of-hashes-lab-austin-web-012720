f092169f96b387c47f6d61d9c13087cfa4014fc2
Reese M Bowman <reesemb1@gmail.com>
2019-12-22 19:51:09 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -56,7 +56,8 @@ def naming_system
               label: "Genus",
               sub_category: {
                 label: "Species",
-                sub_category: {" "}
+                sub_category: {
+                  label: {}
               }
             }
           }
